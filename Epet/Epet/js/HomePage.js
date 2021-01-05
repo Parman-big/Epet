ae5b2a85998a936368bcedd6873a25b5c42547b5
Ltfli <qihang2018@qq.com>
2021-01-05 14:16:19 +0800

    完成提交

--- Epet/Epet/js/HomePage.js
+++ Epet/Epet/js/HomePage.js
@@ -5,7 +5,7 @@ var content = document.querySelector('.site .content')
 var sheng = document.querySelector('.site .sheng')
 var city = document.querySelector('.site .city')
 var quyu = document.querySelector('.site .quyu')
-var site=document.getElementById('site');
+var site = document.getElementById('site');
 // console.log(adres);
 var str = '';
 for (key in adres) {
@@ -27,8 +27,8 @@ content.onclick = function (e) {
     if (sdin == 1) {//如果sdin==1 则当前点击的是省份
         apro = e.target.innerHTML;
         sheng.innerHTML = apro;
-        city.innerHTML='请选择';
-        quyu.innerHTML='请选择';
+        city.innerHTML = '请选择';
+        quyu.innerHTML = '请选择';
         var html = ''
         for (i in adres[apro][0]) {
             html += `
@@ -51,8 +51,42 @@ content.onclick = function (e) {
         content.innerHTML = sntu
     } else if (sdin == 3) {//否则点击的是区域
         quyu.innerHTML = e.target.innerHTML
-        site.innerHTML=apro
+        site.innerHTML = apro
     }
 
-console.log(sdin);
+    console.log(sdin);
 }
+
+// 我得E宠
+var mypet = document.getElementById('mypet')
+mypet.onclick = function () {
+    window.location.href = "my.html"
+}
+// 登录
+var buta = document.getElementById('log')
+var reg = document.querySelector('.register')
+var psd = document.getElementById('psd')
+var but = document.getElementById('but')
+psd.onblur = function () {
+
+    if (psd.value.length != 0) {
+        but.style.backgroundColor = '#f03e3e'
+    } else {
+        but.style.backgroundColor = '#999'
+    }
+}
+
+but.onclick = function () {
+    var shop = document.getElementById('shop').value
+    var psd = document.getElementById('psd').value
+    if (shop == 13223721031 && psd == 123456789) {
+        reg.style.display = 'none'
+        window.location.href = "Home.html"
+    }
+    console.log(shop);
+    console.log(psd);
+}
+buta.onclick = function () {
+    reg.style.display = 'block'
+}
+
